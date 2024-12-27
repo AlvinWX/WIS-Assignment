15c2edfa02977f19cece686e1c6879c666622829
LeeWaiXian <lee.wai.xian@gmail.com>
2024-12-27 17:50:43 +0800

    Redeem voucher function

--- page/leewaixian/redeemvoucher.php
+++ page/leewaixian/redeemvoucher.php
@@ -7,20 +7,23 @@ $_SESSION['path_details'] = $fullPath;
 
 $page = req('page', 1);
 require_once '../../lib/SimplePager.php';
-$p = new SimplePager($query, [], 10, $page);
+$p = new SimplePager('SELECT * FROM voucher', [], 10, $page);
 $arr = $p->result;
 
-//Retrieve member cart
 $member_id = $user->member_id; 
 
-$get_cart_stm = $_db -> prepare('SELECT * FROM cart c JOIN member m ON m.member_id = c.member_id WHERE c.member_id = ?');
-$get_cart_stm -> execute([$member_id]);
-$shoppingCart = $get_cart_stm -> fetch();
+//Retrieve member voucher_list
+$get_voucherlist_stm = $_db -> prepare('SELECT * FROM voucher_list v JOIN member m ON m.member_id = v.member_id WHERE v.member_id = ?');
+$get_voucherlist_stm -> execute([$member_id]);
+$voucher_list = $get_voucherlist_stm -> fetch();
 
-//Retrieve member wishlist
-$get_wishlist_stm = $_db -> prepare('SELECT * FROM wishlist w JOIN member m ON m.member_id = w.member_id WHERE w.member_id = ?');
-$get_wishlist_stm -> execute([$member_id]);
-$wishlist = $get_wishlist_stm -> fetch();
+//Retrieve member points
+$get_updated_points_stm = $_db->prepare('SELECT member_points FROM member WHERE member_id = ?');
+$get_updated_points_stm->execute([$member_id]);
+$updated_member_details = $get_updated_points_stm->fetch();
+
+$points = $updated_member_details -> member_points;
+
 
 ?>
 
@@ -30,69 +33,56 @@ $wishlist = $get_wishlist_stm -> fetch();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/productbox.css">
-    <link rel="stylesheet" href="../../css/productsearch.css">
+    <link rel="stylesheet" href="../../css/redeemvoucher.css">
     <link rel="stylesheet" href="../../css/flash.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="../../js/wishlist.js" defer></script>
+    <script src="../../js/redeemvoucher.js" defer></script>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <title>Search Product <?=$searchString ?></title>
 </head>
 <body>
     <div id="info"><?= temp('info')?></div>
-    <form class="searchmenu">
-        <div class="searchbar"><?= html_search('product_name', 'placeholder=\'Search your product here...\'') ?></div>
-        <div class="categoryselect"><?= html_select('category_id', $_categories, 'All') ?></div>
-        <div class="text"> RM </div>
-        <div class="minpricerange"><?= html_number('minprice', 0, 1000, 1) ?></div>
-        <div class="text"> - RM </div>
-        <div class="maxpricerange"><?= html_number('maxprice', 0, 1000, 1) ?></div>
-        <div class="searchbutton"><button>Search Products</button></div>
-    </form>
+    <div class="empty-box"></div>
 
     <section class="products" id="products">
         <div class="heading">
-            <h1>Search Results: <?=$searchString ?> </h1>
-            <?= $p->count ?> of <?= $p->item_count ?> product(s) |
-            Page <?= $p->page ?> of <?= $p->page_count ?>
+            <h1>Redeem Vouchers </h1>
         </div>
 
-        <section class="sort">
-            <p>Sort by:</p><?= sort_buttons($productName, $productCategory, $minPrice, $maxPrice, $fields, $sort, $dir , "page=$page")?>
-        </section>
+        <div class="amount">
+            <i class='bx bxs-star'></i>Total points: <?= $points ?> point(s)
+        </div>
+
+        <div>
+        <?= $p->count ?> of <?= $p->item_count ?> voucher(s) available to redeem |
+        Page <?= $p->page ?> of <?= $p->page_count ?>
+        </div>
 
         <div class="paging">
-            <p>Select page:</p><?= $p->html("product_name=$productName&category_id=$productCategory&minprice=$minPrice&maxprice=$maxPrice&sort=$sort&dir=$dir") ?>
+            <p>Select page:</p><?= $p->html() ?>
         </div>
     
         <div class="products-container">
         <?php foreach ($arr as $s): ?>
             <div class="box">
-                <img src="../../images/product_pic/<?= $s->product_cover ?>" data-get="productinfo.php?id=<?= $s->product_id ?>">
-                <span data-get="productinfo.php?id=<?= $s->product_id ?>"><?= $s->category_name?></span>
-                <h2 class="product-name" data-get="productinfo.php?id=<?= $s->product_id ?>"><?= $s->product_name?></h2>
+                <img src="../../images/voucher_pic/<?= $s->voucher_img ?>" data-get="voucherinfo.php?id=<?= $s->voucher_id ?>">
+                <span data-get="voucherinfo.php?id=<?= $s->voucher_id ?>">Voucher</span>
+                <h2 class="product-name" data-get="voucherinfo.php?id=<?= $s->product_id ?>"><?= $s->voucher_name?></h2>
                 <?php 
-                    $get_cart_product = $_db->prepare('SELECT * FROM cart_product WHERE product_id = ? AND cart_id = ?');
-                    $get_cart_product -> execute([$s->product_id, $shoppingCart->cart_id]);
-                    $cartProductFound = $get_cart_product -> fetch();
-                    if($cartProductFound != null){ ?>
-                        <h2 class="selected" data-get="productinfo.php?id=<?= $s->product_id ?>">Selected: <?= $cartProductFound->quantity?></h2>
+                    $get_voucher_owned = $_db->prepare('SELECT * FROM voucher_owned WHERE voucher_list_id = ? AND voucher_discount = ? AND voucher_min_spend = ?');
+                    $get_voucher_owned -> execute([$voucher_list -> voucher_list_id, $s -> voucher_discount, $s -> voucher_min_spend]);
+                    $voucherFound = $get_voucher_owned -> fetch();
+                    if($voucherFound != null){ ?>
+                        <h2 class="owned" data-get="voucherinfo.php?id=<?= $s->product_id ?>">Owned: <?= $voucherFound -> voucher_quantity ?></h2>
                 <?php  } else { ?>
-                        <h2 class="selected"></h2>
+                        <h2 class="owned"></h2>
                     <?php  } ?>
-                <h3 class="price" data-get="productinfo.php?id=<?= $s->product_id ?>">RM <?= sprintf('%.2f', $s->product_price) ?></h3>
-                    <a class= "add-to-cart" href="addquantity.php?id=<?= $s->product_id ?>"><i class="bx bx-cart-alt"></i></a>
-                    <?php
-                        $check_wishlist_stm = $_db->prepare('SELECT COUNT(*) FROM wishlist_product WHERE wishlist_id = ? AND product_id = ?');
-                        $check_wishlist_stm->execute([$wishlist->wishlist_id, $s->product_id]);
-                        $isWished = $check_wishlist_stm->fetchColumn() == 0 ? false : true;
-                    ?>
-                    <svg class='bx bx-heart' viewBox='0 0 24 24' width='24' height='24' onclick="updateWishlist('<?= $s->product_id ?>', '<?= $isWished ? 'remove' : 'add' ?>', '<?= $wishlist->wishlist_id ?>' , this)">
-                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="<?= $isWished ? '#ff007f' : 'none' ?>" stroke="#ff007f" stroke-width="2"/>
-                    </svg>
-                <span class="sold" data-get="productinfo.php?id=<?= $s->product_id ?>"><?= $s->product_sold?> sold || <?= $s->product_stock?> left</span>
+                <h3 class="price" data-get="voucherinfo.php?id=<?= $s->voucher_id ?>"><?= $s -> voucher_points ?> points</h3>
+                <button class="redeem" onclick="confirmRedeem(<?= $points ?>,  <?= $s -> voucher_points ?>, '<?= $s->voucher_id ?>', '<?= $voucher_list -> voucher_list_id ?>', '<?= $fullPath ?>')"><i class='bx bxs-gift'></i></button>
+                <!-- <a class= "add-to-cart" href="addquantity.php?id=<?= $s->voucher_id ?>"><i class='bx bxs-gift'></i></a> -->
             </div>
             <?php endforeach ?>
             </div>
