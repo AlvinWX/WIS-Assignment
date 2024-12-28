36adf8ae3096171cfbc82cbc71ff36e52513f775
yijingchan12 <eugenechan717@gmail.com>
2024-12-29 00:17:10 +0800

    edited

--- page/chanyijing/admin/admin_management/admin_detail.php
+++ page/chanyijing/admin/admin_management/admin_detail.php
@@ -30,27 +30,27 @@ include '../../../../_head.php';
     </tr>
     <tr>
         <th>Admin ID</th>
-        <td><?= $s->admin_id ?></td>
+        <td colspan="2"><?= $s->admin_id ?></td>
     </tr>
     <tr>
         <th>Name</th>
-        <td><?= $s->admin_name ?></td>
+        <td colspan="2"><?= $s->admin_name ?></td>
     </tr>
     <tr>
         <th>Tier</th>
-        <td><?= $s->admin_tier ?></td>
+        <td colspan="2"><?= $s->admin_tier ?></td>
     </tr>
     <tr>
         <th>Email</th>
-        <td><a href="mailto:<?= $s->admin_email ?>"><?= $s->admin_email ?></a></td>
+        <td colspan="2"><a href="mailto:<?= $s->admin_email ?>"><?= $s->admin_email ?></a></td>
     </tr>
     <tr>
         <th>Phone</th>
-        <td><a href="tel:<?= $s->admin_phone ?>"><?= $s->admin_phone ?></a></td>
+        <td colspan="2"><a href="tel:<?= $s->admin_phone ?>"><?= $s->admin_phone ?></a></td>
     </tr>
     <tr>
         <th>Gender</th>
-        <td><?= $s->admin_gender ?></td>
+        <td colspan="2"><?= $s->admin_gender ?></td>
     </tr>
 </table>
 
